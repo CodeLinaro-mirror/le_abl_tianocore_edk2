fc5cd1dd23aa9373d3b7da72b681bbc439004586
Nicolas Dechesne <nicolas.dechesne@linaro.org>
2019-06-19 18:55:47 +0200

    QcomModulePkg: BootLib: skip ValidateSlotGuids()
    When using community builds, we want to depend as less as possible on
    Qualcomm internal builds quirks. This function does a few checks which
    are relevant when booting a Qualcomm Android image, but not well
    suited for community builds. For example users might choose to remove
    system_x partition, in which case the bootloader wouldn't boot
    anymore.
    
    Signed-off-by: Nicolas Dechesne <nicolas.dechesne@linaro.org>

--- QcomModulePkg/Library/BootLib/PartitionTableUpdate.c
+++ QcomModulePkg/Library/BootLib/PartitionTableUpdate.c
@@ -1111,6 +1111,7 @@ UpdatePartitionTable (UINT8 *GptImage,
   return Status;
 }
 
+#if 0
 STATIC CONST struct PartitionEntry *
 GetPartitionEntry (CHAR16 *Partition)
 {
@@ -1124,6 +1125,7 @@ GetPartitionEntry (CHAR16 *Partition)
   }
   return &PtnEntries[Index];
 }
+#endif
 
 STATIC struct PartitionEntry *
 GetBootPartitionEntry (Slot *BootSlot)
@@ -1471,6 +1473,7 @@ EFI_STATUS ClearUnbootable (VOID)
   return EFI_SUCCESS;
 }
 
+#if 0
 STATIC EFI_STATUS
 ValidateSlotGuids (Slot *BootableSlot)
 {
@@ -1533,6 +1536,7 @@ ValidateSlotGuids (Slot *BootableSlot)
   DEBUG ((EFI_D_INFO, "Booting from slot (%s)\n", BootableSlot->Suffix));
   return EFI_SUCCESS;
 }
+#endif
 
 EFI_STATUS
 FindBootableSlot (Slot *BootableSlot)
@@ -1589,10 +1593,12 @@ FindBootableSlot (Slot *BootableSlot)
     GUARD_OUT (HandleActiveSlotUnbootable ());
   }
 
+#if 0
   /* Validate slot suffix and partition guids */
   if (Status == EFI_SUCCESS) {
     GUARD_OUT (ValidateSlotGuids (BootableSlot));
   }
+#endif
   MarkPtnActive (BootableSlot->Suffix);
 out:
   if (Status != EFI_SUCCESS) {
