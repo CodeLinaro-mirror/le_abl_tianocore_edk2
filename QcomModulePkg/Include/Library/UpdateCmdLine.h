e67addafb364384b87b8b72e90fe680921a8b1e1
Nagireddy Annem <nannem@codeaurora.org>
2019-12-10 18:01:03 +0530

    QcomModulePkg: Update CmdLine with systemd ffbm mode
    Update CmdLine with systemd ffbm mode for LE variant targets.
    
    Change-Id: Ifdd7b4ff3411d222bd92efaeb23410bc2aabc02c

--- QcomModulePkg/Include/Library/UpdateCmdLine.h
+++ QcomModulePkg/Include/Library/UpdateCmdLine.h
@@ -3,7 +3,7 @@
  * Copyright (c) 2009, Google Inc.
  * All rights reserved.
  *
- * Copyright (c) 2009-2019, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2009-2020, The Linux Foundation. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
@@ -81,6 +81,7 @@ typedef struct UpdateCmdLineParamList {
   CHAR8 *LEVerityCmdLine;
   CHAR8 *CvmSystemPtnCmdLine;
   CONST CHAR8 *SystemdSlotEnv;
+  CONST CHAR8 *SystemdFfbmMode;
 } UpdateCmdLineParamList;
 
 EFI_STATUS
