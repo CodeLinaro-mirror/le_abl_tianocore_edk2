a696b2e87b35049450b1cc5bd04770e3d0743ff1
Nagireddy Annem <nannem@codeaurora.org>
2020-01-30 16:02:15 +0530

    QcomModulePkg: Add new macro for systemd bootslot commandline
    Add new macro for appending systemd bootslot commandline to
    avoid default addition for all LE targets with existing macro.
    
    Change-Id: I75f0a58a32b04e04a319b301948910fd4d366af2

--- QcomModulePkg/Include/Library/BootLinux.h
+++ QcomModulePkg/Include/Library/BootLinux.h
@@ -1,4 +1,4 @@
-/* Copyright (c) 2015-2019, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2015-2020, The Linux Foundation. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are
@@ -210,4 +210,5 @@ BOOLEAN IsABRetryCountDisabled (VOID);
 BOOLEAN IsDynamicPartitionSupport (VOID);
 UINT64 SetandGetLoadAddr (BootParamlist *BootParamlistPtr, AddrType Type);
 BOOLEAN IsNANDSquashFsSupport (VOID);
+BOOLEAN IsSystemdBootslotEnabled (VOID);
 #endif
