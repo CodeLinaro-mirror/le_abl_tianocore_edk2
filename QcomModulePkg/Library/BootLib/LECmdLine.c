cc3d5a32064046bbace825c193ef875be83ca098
Hanumantha Reddy Naradla <quic_hnaradla@quicinc.com>
2022-08-04 16:06:22 +0530

    dm-verity: ufs: Extend dm-verity support onto UFS
    dm-verity is supported on emmc and NAND.
    Extend it to UFS, as well.
    
    Change-Id: Ib97f5a95f3082202b42be804eec8183164d25183

--- QcomModulePkg/Library/BootLib/LECmdLine.c
+++ QcomModulePkg/Library/BootLib/LECmdLine.c
@@ -26,6 +26,40 @@
  * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
  * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  *
+ * Changes from Qualcomm Innovation Center are provided under the following license:
+ *
+ * Copyright (c) 2022 Qualcomm Innovation Center, Inc. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted (subject to the limitations in the
+ * disclaimer below) provided that the following conditions are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *
+ *     * Redistributions in binary form must reproduce the above
+ *       copyright notice, this list of conditions and the following
+ *       disclaimer in the documentation and/or other materials provided
+ *       with the distribution.
+ *
+ *     * Neither the name of Qualcomm Innovation Center, Inc. nor the names of its
+ *       contributors may be used to endorse or promote products derived
+ *       from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE
+ * GRANTED BY THIS LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT
+ * HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+ * GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+ * IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
+ * IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
  **/
 
 #include <Library/PartitionTableUpdate.h>
@@ -41,6 +75,8 @@
 #define MAX_VERITY_SYSTEM_PARTITION_STR_LEN 20
 STATIC CONST CHAR8 *VeritySystemPartitionStrEmmc = "/dev/mmcblk0p";
 STATIC CONST CHAR8 *VeritySystemPartitionStrNand = "/dev/ubiblock0_0";
+STATIC CONST CHAR8 *VeritySystemPartitionStrUFS = "/dev/sd";
+
 STATIC CONST CHAR8 *VerityName = "verity";
 STATIC CONST CHAR8 *VerityAppliedOn = "system";
 STATIC CONST CHAR8 *VerityEncriptionName = "sha256";
@@ -163,6 +199,8 @@ GetLEVerityCmdLine (CONST CHAR8 *SourceCmdLine,
   BOOLEAN MultiSlotBoot = FALSE;
   CHAR16 PartitionName[MAX_GPT_NAME_SIZE];
   INT32 Index = 0;
+  CHAR8 LunCharMapping[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'};
+  UINT32 Lun = 0;
 
   /* Get verity command line from SourceCmdLine */
   DMDataStr = AsciiStrStr (SourceCmdLine, "verity=");
@@ -260,6 +298,8 @@ GetLEVerityCmdLine (CONST CHAR8 *SourceCmdLine,
       goto ErrLEVerityout;
     }
 
+    Lun = GetPartitionLunFromIndex (Index);
+
     CHAR8 VeritySystemPartitionStr[MAX_VERITY_SYSTEM_PARTITION_STR_LEN];
     CHAR8 RootDevStr[BOOT_DEV_NAME_SIZE_MAX];
     GetRootDeviceType (RootDevStr, BOOT_DEV_NAME_SIZE_MAX);
@@ -286,6 +326,11 @@ GetLEVerityCmdLine (CONST CHAR8 *SourceCmdLine,
       "%a",
       VeritySystemPartitionStrNand
       );
+    } else if (!AsciiStrCmp ("UFS", RootDevStr)) {
+      AsciiSPrint (
+      VeritySystemPartitionStr,
+      MAX_VERITY_SYSTEM_PARTITION_STR_LEN,
+      "%a%c%d", VeritySystemPartitionStrUFS, LunCharMapping[Lun], Index);
     } else {
       /* Append index to EMMC partition string */
       AsciiSPrint (
