863102fe71e66973b6b055a125566b05c1227aff
Auditya Bhattaram <audityab@qti.qualcomm.com>
2023-02-22 04:41:47 -0800

    edk2: QcomModulePkg: Increment EFIChip APIs to version 4
    Increment EFIChip APIs to version 4
      1. Newly added APIs are EFI_DALCHIPINFO_GETSKU,
         EFI_CHIPINFO_GETNUMFUNCTIONALCLUSTERS &
         EFI_CHIPINFO_GETBOOTCLUSTERANDCORE
    
      2. Added Parts are EFICHIPINFO_PART_COMPUTE_1 &
         EFICHIPINFO_PART_DISPLAY_1
    
    Change-Id: Id4ca46b477550f81fb3b5fe0754d759d4b76dfb3

--- QcomModulePkg/Include/Protocol/EFIChipInfo.h
+++ QcomModulePkg/Include/Protocol/EFIChipInfo.h
@@ -27,39 +27,9 @@
 */
 
 /*
- * Changes from Qualcomm Innovation Center are provided under the following license:
- *
- * Copyright (c) 2022 Qualcomm Innovation Center, Inc. All rights reserved.
- *
- *  Redistribution and use in source and binary forms, with or without
- *  modification, are permitted (subject to the limitations in the
- *  disclaimer below) provided that the following conditions are met:
- *
- *      * Redistributions of source code must retain the above copyright
- *        notice, this list of conditions and the following disclaimer.
- *
- *      * Redistributions in binary form must reproduce the above
- *        copyright notice, this list of conditions and the following
- *        disclaimer in the documentation and/or other materials provided
- *        with the distribution.
- *
- *      * Neither the name of Qualcomm Innovation Center, Inc. nor the names of its
- *        contributors may be used to endorse or promote products derived
- *        from this software without specific prior written permission.
- *
- *  NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE
- *  GRANTED BY THIS LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT
- *  HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED
- *  WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
- *  MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
- *  IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
- *  ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- *  DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
- *  GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- *  INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
- *  IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
- *  OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
- *  IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * Changes from Qualcomm Technologies, Inc. are provided under the following license:
+ * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+ * SPDX-License-Identifier: BSD-3-Clause-Clear
  */
 
 #ifndef __EFICHIPINFO_H__
@@ -76,7 +46,10 @@ typedef struct _EFI_CHIPINFO_PROTOCOL EFI_CHIPINFO_PROTOCOL;
 /**
   Protocol version.
 */
-#define EFI_CHIPINFO_PROTOCOL_REVISION 0x0000000000010002
+#define EFI_CHIPINFO_PROTOCOL_REVISION_3  0x0000000000010003
+#define EFI_CHIPINFO_PROTOCOL_REVISION_4  0x0000000000010004
+
+#define EFI_CHIPINFO_PROTOCOL_REVISION EFI_CHIPINFO_PROTOCOL_REVISION_4
 /** @} */ /* end_addtogroup efi_chipInfo_constants */
 
 /*  Protocol GUID definition */
@@ -484,6 +457,84 @@ typedef EFI_STATUS (EFIAPI *EFI_DALCHIPINFO_GETSUBSETCPUS) (
     IN EFI_CHIPINFO_PROTOCOL *This,
     IN UINT32 nCPUCluster,
     OUT UINT32 *pnMask);
+/* ============================================================================
+**  Function : EFI_DalChipInfo_GetSKU
+** ============================================================================
+*/
+/** @ingroup efi_chipInfo_protocol_apis
+ * @par Summary
+ * Get SKU and Product Code information for the current device
+ *
+ * @param[in]   This          Pointer to the EFI_CHIPINFO_PROTOCOL instance
+ * @param[out]  pInfo         pointer to a caller-allocated buffer where SKU
+ *                            information will be stored
+ *
+ * @return
+ * EFI_SUCCESS            -- SKU information was stored in pInfo successfully \n
+ * EFI_INVALID_PARAMETER  -- pInfo was invalid \n
+ * EFI_UNSUPPORTED        -- SKU information is not available \n
+ * EFI_PROTOCOL_ERROR     -- Other errors
+ */
+typedef
+EFI_STATUS
+(EFIAPI *EFI_DALCHIPINFO_GETSKU)(
+    IN EFI_CHIPINFO_PROTOCOL *This,
+    OUT EFIChipInfoSKUType *pInfo
+    );
+
+/* ============================================================================
+**  Function : EFI_ChipInfo_GetNumFunctionalClusters
+** ============================================================================
+*/
+/** @ingroup efi_chipInfo_protocol_apis
+ * Get the number of functional clusters.
+ *
+ * This is the total number of functional clusters based on partial
+ * binning and SKUing. A cluster is deemed functional if it has at least
+ * one functional core. There will always be at least 1 functional
+ * cluster: the one that's running this code.
+ *
+ * @param[in]  This             Pointer to the EFI_CHIPINFO_PROTOCOL instance
+ * @param[out] pnNumClusters    buffer to store the number of clusters
+ *
+ * @return
+ * EFI_SUCCESS            -- the number of clusters was successfully retrieved\n
+ *                            and stored in pnNumClusters; \n
+ * EFI_INVALID_PARAMETER  -- pnNumClusters is invalid; \n
+ * EFI_NOT_READY          -- this function was called before EFIChipInfo has \n
+ *                            initialized. *pnNumClusters will not be updated.
+ */
+typedef
+EFI_STATUS
+(EFIAPI *EFI_CHIPINFO_GETNUMFUNCTIONALCLUSTERS)(
+    IN EFI_CHIPINFO_PROTOCOL *This,
+    OUT UINT32 *pnNumClusters
+    );
+
+/* ============================================================================
+**  Function : EFI_ChipInfo_GetBootClusterAndCore
+** ============================================================================
+*/
+/** @ingroup efi_chipInfo_protocol_apis
+ * Get the boot cluster and core.
+ *
+ * @param[in]   This          Pointer to the EFI_CHIPINFO_PROTOCOL instance
+ * @param[out] pnCluster buffer to store the boot cluster index (zero-indexed).
+ * @param[out] pnCore      buffer to store the boot core index (zero-indexed).
+ *
+ * @return
+ * EFI_SUCCESS            -- both pointers were filled correctly; \n
+ * EFI_INVALID_PARAMETER  -- either pointer is invalid; \n
+ * EFI_NOT_READY          -- this function was called before EFIChipInfo has \n
+ *                          initialized. Neither pointer will be updated.
+ */
+typedef
+EFI_STATUS
+(EFIAPI *EFI_CHIPINFO_GETBOOTCLUSTERANDCORE)(
+    IN EFI_CHIPINFO_PROTOCOL *This,
+    OUT UINT32 *pnCluster,
+    OUT UINT32 *pnCore
+    );
 
 /*===========================================================================
   PROTOCOL INTERFACE
@@ -513,6 +564,9 @@ struct _EFI_CHIPINFO_PROTOCOL {
   EFI_DALCHIPINFO_GETMARKETINGNAMESTRING GetMarketingNameString;
   EFI_DALCHIPINFO_GETSUBSETPART GetSubsetPart;
   EFI_DALCHIPINFO_GETSUBSETCPUS GetSubsetCPUs;
+  EFI_DALCHIPINFO_GETSKU GetSKU;
+  EFI_CHIPINFO_GETNUMFUNCTIONALCLUSTERS GetNumFunctionalClusters;
+  EFI_CHIPINFO_GETBOOTCLUSTERANDCORE GetBootClusterAndCore;
 };
 
 #endif /* __EFICHIPINFO_H__ */
