db39f99526a300a2d0d0d9d1dbbe6875e8158599
Akshay Adiga <quic_aadiga@quicinc.com>
2023-04-18 23:06:04 -0700

    QcomModulePkg: Add support for GetActiveSlot for RecoveryInfo
    Use UEFI call to get GetActiveSlot.
    
    For lemans PVM, recoveryinfo partition is used for fallback
    when abl fails, so legacy GPT attribute based method also
    remains.
    
    Change-Id: Id68ce864a4bab4d565156a08d09c450c2c0869c8
    (cherry picked from commit fd9b296bf4c5ba3052328b6276a687a155280823)

--- QcomModulePkg/Library/BootLib/RecoveryInfo.c
+++ QcomModulePkg/Library/BootLib/RecoveryInfo.c
@@ -42,3 +42,32 @@ BOOLEAN IsRecoveryInfo ()
 
   return (HasRecoveryInfo == 1);
 }
+
+EFI_STATUS RI_GetActiveSlot (Slot *ActiveSlot)
+{
+  EFI_STATUS Status = EFI_SUCCESS ;
+  EFI_RECOVERYINFO_PROTOCOL *pRecoveryInfoProtocol = NULL;
+  BootSetType BootSet;
+  Slot Slots[] = {{L"_a"}, {L"_b"}};
+
+  Status = gBS->LocateProtocol (& gEfiRecoveryInfoProtocolGuid, NULL,
+                               (VOID **) & pRecoveryInfoProtocol);
+  if (Status != EFI_SUCCESS) {
+    return Status;
+  }
+
+  Status = pRecoveryInfoProtocol -> GetBootSet (pRecoveryInfoProtocol,
+                                                &BootSet);
+  if (Status != EFI_SUCCESS ||
+      BootSet == SET_INVALID) {
+    DEBUG ((EFI_D_ERROR, "GetBootSet : Error returned %d", Status ));
+    return Status;
+  }
+
+  /* SET_A = 0 SET_B = 1 */
+   GUARD (StrnCpyS (ActiveSlot->Suffix, ARRAY_SIZE (ActiveSlot->Suffix),
+                    Slots[BootSet].Suffix,
+                    StrLen (Slots[BootSet].Suffix)));
+   return EFI_SUCCESS;
+
+}
