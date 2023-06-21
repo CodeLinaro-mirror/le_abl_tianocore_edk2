6ddcd2c06857664855ee5ed4e1cea48c5689b740
Akshay Adiga <quic_aadiga@quicinc.com>
2023-06-20 20:54:13 -0700

    QcomModulePkg: Check Recoveryinfo state in IsRecoveryInfo
    Availability of EFIRecoveryInfo does not guarantee a/b slot switching is
    possible. RecoveryInfo state can be used to determine ability of slot
    switching.
    
    Change-Id: Ib21206eff1e98d0e913abd3dd010d841faf83622
    (cherry picked from commit fa5520fcb97ca82d55484641ebaa771c85993720)

--- QcomModulePkg/Library/BootLib/RecoveryInfo.c
+++ QcomModulePkg/Library/BootLib/RecoveryInfo.c
@@ -8,6 +8,8 @@
 #include <Library/DebugLib.h>
 #include <Protocol/EFIRecoveryInfo.h>
 #include <Library/UefiBootServicesTableLib.h>
+#include "PartitionTableUpdate.h"
+#include <VerifiedBoot.h>
 
 
 STATIC INT64 HasRecoveryInfo = -1;
@@ -16,14 +18,25 @@ BOOLEAN IsRecoveryInfo ()
 {
   EFI_STATUS Status = EFI_SUCCESS ;
   EFI_RECOVERYINFO_PROTOCOL *pRecoveryInfoProtocol = NULL;
+  RECOVERY_STATUS_STATE RecoveryState;
 
   if (HasRecoveryInfo == -1 ) {
+    DEBUG (( EFI_D_VERBOSE,  "Initializing HasRecoveryInfo\n"));
     Status = gBS->LocateProtocol (& gEfiRecoveryInfoProtocolGuid, NULL,
                                   (VOID **) & pRecoveryInfoProtocol);
-    if (Status == EFI_SUCCESS) {
-      HasRecoveryInfo = 1;
-    } else {
+    if (Status != EFI_SUCCESS) {
       HasRecoveryInfo = 0;
+      return FALSE;
+    }
+
+    Status = pRecoveryInfoProtocol -> GetRecoveryState (pRecoveryInfoProtocol,
+                                                        &RecoveryState);
+    if (Status != EFI_SUCCESS) {
+      HasRecoveryInfo = 1;
+    } else if ((RecoveryState != RECOVERY_INFO_PARTITION_FAIL) &&
+         (Status == EFI_SUCCESS)) {
+      DEBUG (( EFI_D_INFO,  "RecoveryInfo is enabled\n"));
+      HasRecoveryInfo = 1;
     }
   }
 
