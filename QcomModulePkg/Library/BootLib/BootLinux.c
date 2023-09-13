f988ab433e3ec69e94015e1caa80461b4ed7cf18
Sankalp Negi <quic_snegi@quicinc.com>
2023-09-13 01:54:38 -0700

    QcomModulePkg: Allow multiple DTBs in Appended DTB Case
    Allow parsing of all DTBs, when more than 1 DTB is
    appended to the kernel image. This is needed when we
    wish to choose DTB based on platform type.
    
    Change-Id: I8bc62d1a3558aae7ea39f4e25955ad9c83958b94

--- QcomModulePkg/Library/BootLib/BootLinux.c
+++ QcomModulePkg/Library/BootLib/BootLinux.c
@@ -571,11 +571,11 @@ DTBImgCheckAndAppendDT (BootInfo *Info, BootParamlist *BootParamlistPtr)
         NextDtHdr =
           (VOID *)((uintptr_t)SingleDtHdr + fdt_totalsize (SingleDtHdr));
         if (!fdt_check_header (NextDtHdr)) {
-          DEBUG ((EFI_D_VERBOSE, "Not the single appended DTB\n"));
-          return EFI_NOT_FOUND;
+          DEBUG ((EFI_D_VERBOSE, "Multiple appended DTBs found\n"));
+        } else {
+          DEBUG ((EFI_D_VERBOSE, "Single appended DTB found\n"));
         }
 
-        DEBUG ((EFI_D_VERBOSE, "Single appended DTB found\n"));
         if (CHECK_ADD64 (BootParamlistPtr->DeviceTreeLoadAddr,
                                 fdt_totalsize (SingleDtHdr))) {
           DEBUG ((EFI_D_ERROR,
