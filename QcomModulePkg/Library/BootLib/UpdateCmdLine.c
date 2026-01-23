854e7a6cff7349bd65ff7d837e97e675f004e952
Scott Bauer <sbauer@qti.qualcomm.com>
2026-01-23 15:03:40 -0800

    QcomModulePkg: Fix propagation of untrusted input into kernel cmdline
    First lets set the size to prevent any double-fetch in the getVariable.
    Then limit any input to either 1 or 0.
    
    Change-Id: I449f74ddefcec665f4e0ea4ed538f892dfb14e21

--- QcomModulePkg/Library/BootLib/UpdateCmdLine.c
+++ QcomModulePkg/Library/BootLib/UpdateCmdLine.c
@@ -391,6 +391,8 @@ STATIC EFI_STATUS GetGpuCmdline (VOID)
 {
   EFI_STATUS Status;
 
+  GpuCmdLineLen = sizeof (GpuCmdLine);
+
   Status = gRT->GetVariable ((CHAR16 *)L"GpuConfiguration",
                              &gQcomTokenSpaceGuid, NULL, &GpuCmdLineLen,
                              GpuCmdLine);
