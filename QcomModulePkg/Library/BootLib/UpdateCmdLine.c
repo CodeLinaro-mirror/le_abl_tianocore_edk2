c9e5dda33c26c49b0def8fcc11f89aa6de51f9cb
wenjzhan <wenjzhan@qti.qualcomm.com>
2026-02-03 16:34:31 +0800

    QcomModulePkg: fix security vulnerability static varialbe overflow vulnerability
    Always reset the length variable to the actual buffer size immediately before the call.
    
    Change-Id: I25a6571151f6a222c1d5b565e2277b25784dc62c
    (cherry picked from commit aa5f96b72c4d53ec2a752fc760d1f442c8d1bb42)
    (cherry picked from commit ce0187fa37f37dbb1d5390f1d15b92739aa2d08c)

--- QcomModulePkg/Library/BootLib/UpdateCmdLine.c
+++ QcomModulePkg/Library/BootLib/UpdateCmdLine.c
@@ -378,6 +378,7 @@ STATIC VOID GetDisplayCmdline (VOID)
   Status = gRT->GetVariable ((CHAR16 *)L"DisplayPanelConfiguration",
                              &gQcomTokenSpaceGuid, NULL, &DisplayCmdLineLen,
                              DisplayCmdLine);
+  DisplayCmdLineLen = sizeof (DisplayCmdLine);
   if (Status != EFI_SUCCESS) {
     DEBUG ((EFI_D_ERROR, "Unable to get Panel Config, %r\n", Status));
   }
@@ -390,6 +391,7 @@ STATIC VOID GetHwFenceCmdline (VOID)
   Status = gRT->GetVariable ((CHAR16 *)L"HwFenceConfiguration",
                              &gQcomTokenSpaceGuid, NULL, &HwFenceCmdLineLen,
                              HwFenceCmdLine);
+  HwFenceCmdLineLen = sizeof (HwFenceCmdLine);
   if (Status != EFI_SUCCESS) {
     DEBUG ((EFI_D_ERROR, "Unable to get hw fence Config, %r\n", Status));
   }
@@ -404,6 +406,7 @@ STATIC EFI_STATUS GetGpuCmdline (VOID)
   Status = gRT->GetVariable ((CHAR16 *)L"GpuConfiguration",
                              &gQcomTokenSpaceGuid, NULL, &GpuCmdLineLen,
                              GpuCmdLine);
+  GpuCmdLineLen = sizeof (GpuCmdLine);
   if (Status != EFI_SUCCESS) {
     DEBUG ((EFI_D_ERROR, "Unable to get GPU Preempt Config, %r\n", Status));
   }
