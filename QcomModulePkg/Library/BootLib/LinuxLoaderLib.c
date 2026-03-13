c74288b4ef42284f07366e8f90dbcab4f4cc8acb
Varun Sai Bonam <vbonam@qti.qualcomm.com>
2026-03-13 11:51:18 +0530

    ABL: Fix SD card boot flow with latest patch
    The EDK2 fix corrects the SD boot decision logic where
    LoadImageFromFatPartition() was being invoked unconditionally, even
    when IsSdCardPresent() handling should gate the SD boot path.
    
    Change-Id: I51d257e234d8a5fc7f594eab4e001607edb4d6e7
    Signed-off-by: Varun Sai Bonam <vbonam@qti.qualcomm.com>
    (cherry picked from commit 45d68ac6843a3c5bd2f34c96381f361309eb0ffc)

--- QcomModulePkg/Library/BootLib/LinuxLoaderLib.c
+++ QcomModulePkg/Library/BootLib/LinuxLoaderLib.c
@@ -321,8 +321,6 @@ LoadImageFromFatPartition (VOID *ImageBuffer, UINT32 *ImageSize, CHAR16 *Pname)
 
 EFI_FILE_HANDLE UpdateDirHandle = NULL;
 
-if (IsSdCardPresent())
-{
     Status = RootFileHandle->Open(RootFileHandle, &UpdateDirHandle, (CHAR16*)L"update",
                                   EFI_FILE_MODE_READ, 0);
 
@@ -343,18 +341,6 @@ if (IsSdCardPresent())
     }
 
     UpdateDirHandle->Close(UpdateDirHandle);
-}
-else
-{
-    Status = RootFileHandle->Open(RootFileHandle, &FileHandle, (CHAR16*)L"boot.img",
-                                  (UINT64)EFI_FILE_MODE_READ, 0);
-
-    if ((Status != EFI_SUCCESS) || (FileHandle == NULL))
-    {
-        DEBUG ((DEBUG_ERROR, "Failed to open root file handle Status :%d\n", Status));
-        return Status;
-    }
-}
 
   Size = ROUND_TO_PAGE(*ImageSize, (HandleInfoList[1].BlkIo->Media->BlockSize - 1));
 
@@ -385,19 +371,7 @@ LoadImageFromPartition (VOID *ImageBuffer, UINT32 *ImageSize, CHAR16 *Pname)
   STATIC UINT32 MaxHandles;
   STATIC UINT32 BlkIOAttrib = 0;
 
-
-if (IsSdCardPresent())
-{
-    if (StrStr(Pname, L"recovery"))
-    {
-        // SD card recovery image case
-    }
-    else if (StrStr(Pname, L"boot"))
-    {
-        // Normal boot image case
-    }
-}
-
+  if (IsSdCardPresent() && StrStr(Pname, L"recovery"))
   {
      Status = LoadImageFromFatPartition(ImageBuffer, ImageSize, Pname);
      if (Status == EFI_SUCCESS)
